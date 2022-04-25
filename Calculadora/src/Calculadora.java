48ad894a493568c65e837b11f470a17503217f9c
Eduardo C. Vaz <61214197+eduardocvaz@users.noreply.github.com>
2022-04-25 13:46:02 -0300

    06-calculadora

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,42 @@
+
+public class Calculadora {
+	
+	public double somar() {
+		return 0;
+		
+	}
+	
+	public double subtrair(int x, int y) {
+		return x+y;
+		
+	}
+	
+	public double multiplicar(int x, int y) {
+		return x*y;
+		
+	}
+	
+	public double dividir(int x, int y) {
+		return x/y;
+		
+	}
+	
+	public double dividirUmPor(int x) {
+		return 1/x;
+		
+	}
+	
+	public double elevarPorDois(int x) {
+		return x*x;
+		
+	}
+	
+	public double raizDe(int x) {
+		return Math.sqrt(x);
+		
+	}
+	
+	
+	
+	
+}
